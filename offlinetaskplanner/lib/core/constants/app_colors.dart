640ea938f9d10d0e9295f1cbb5a50fdf8aa87b49
Nguyen Van Thinh <nguyenvanthinh3434mc2@gmail.com>
2025-12-20 12:15:56 +0700

    Create flutter app

---
+++ offlinetaskplanner/lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromARGB(255, 115, 201, 216);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color textPrimary = Color(0xFF000000);
+}
